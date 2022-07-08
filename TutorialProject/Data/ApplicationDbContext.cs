293caeb4874fe7c32357882c00189b0dd8665d0f
R M Shahidul Islam Shahed <shahedbddev@gmail.com>
2022-07-08 11:40:14 +0600

    + complete crud operation done

---
+++ TutorialProject/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using TutorialProject.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace TutorialProject.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+        public DbSet<Category> Category { get; set; }
+
+    }
+}
---
+++ TutorialProject/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TutorialProject.Models
+{
+    public class Category
+    {
+        public Int64 Id { get; set; }
+        [Required]
+        public string? Name { get; set; }
+        [Required]
+        public string? Description { get; set; }
+    }
+}
